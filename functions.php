4596ce32aea367b6603265bd50de34a3fa08de97
jeremyfelt <jeremy.felt@gmail.com>
2015-03-30 13:50:16 -0700

    Enqueue the WSU Home styles before the child theme styles

--- functions.php
+++ functions.php
@@ -8,7 +8,7 @@ class WSU_Timeline_Theme {
 	 * Setup hooks for the theme.
 	 */
 	public function __construct() {
-
+		add_action( 'spine_enqueue_styles', array( $this, 'enqueue_styles' ) );
 	}
 
 	public function get_timeline_items() {
@@ -19,6 +19,15 @@ class WSU_Timeline_Theme {
 		$query = new WP_Query( $args );
 		wp_reset_query();
 		return $query;
+	}
+
+	/**
+	 * Load the stylesheet from the main wsu.edu theme before the child theme.
+	 */
+	public function enqueue_styles() {
+		wp_dequeue_style( 'spine-theme-child' );
+		wp_enqueue_style( 'wsu-home-style', 'https://wsu.edu/wp-content/themes/wsu-home/style.css', array(), spine_get_script_version() );
+		wp_enqueue_style( 'spine-theme-child' );
 	}
 }
 $wsu_timeline_theme = new WSU_Timeline_Theme();

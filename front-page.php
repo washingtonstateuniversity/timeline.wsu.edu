c3b360b38885c4b6bd27bf07f753ce01482f9fed
jeremyfelt <jeremy.felt@gmail.com>
2015-04-20 16:55:38 -0700

    Wrap the inside structure of .timeline-item-container
    Use `.timeline-item-internal-wrapper`

--- front-page.php
+++ front-page.php
@@ -26,27 +26,29 @@ while( $timeline_query->have_posts() ) {
 	$item_has_featured_image = spine_has_featured_image();
 	?>
 	<div class="column <?php echo $column_class; ?> timeline-item-container <?php if ( $item_has_featured_image ) : echo 'item-has-featured-image'; endif; ?>">
-		<header>
-			<h2><?php the_title(); ?></h2>
-			<?php if ( ! empty( $timeline_sub_headline ) ) : ?><h3><?php echo $timeline_sub_headline; ?></h3><?php endif; ?>
-		</header>
-		<?php
-		if ( $item_has_featured_image ) {
-			echo '<figure class="timeline-featured-image"><img src="' . esc_url( spine_get_featured_image_src( 'spine-small_size' ) ) . '"></figure>';
-		}
-		?>
-		<span class="start-date"><?php echo esc_html( $start_date ); ?></span>
-		<?php if ( ! empty( $end_date ) ) : ?><span class="end-date"><?php echo esc_html( $end_date ); ?></span><?php endif; ?>
-		<?php if ( ! empty( $external_url ) ) : ?><span class="external-url"><a href="<?php echo esc_url( $external_url ); ?>"><?php echo esc_url( $external_url ); ?></a></span><?php endif; ?>
-		<div class="timeline-content timeline-content-<?php echo $column_class; ?>">
+		<div class="timeline-item-internal-wrapper">
+			<header>
+				<h2><?php the_title(); ?></h2>
+				<?php if ( ! empty( $timeline_sub_headline ) ) : ?><h3><?php echo $timeline_sub_headline; ?></h3><?php endif; ?>
+			</header>
 			<?php
-			$content = apply_filters( 'the_content', get_the_content() );
-			$content = preg_replace( '@<(script|style)[^>]*?>.*?</\\1>@si', '', $content );
-			$content = strip_tags( $content, '<p><a><span><div><strong><em><b><i><sup><sub><ul><li><h1><h2><h3><h4><h5><h6>' );
-			$content = str_replace( '<p>&nbsp;</p>', '', $content );
-			$content = str_replace( '<p></p>', '', $content );
-			echo $content;
+			if ( $item_has_featured_image ) {
+				echo '<figure class="timeline-featured-image"><img src="' . esc_url( spine_get_featured_image_src( 'spine-small_size' ) ) . '"></figure>';
+			}
 			?>
+			<span class="start-date"><?php echo esc_html( $start_date ); ?></span>
+			<?php if ( ! empty( $end_date ) ) : ?><span class="end-date"><?php echo esc_html( $end_date ); ?></span><?php endif; ?>
+			<?php if ( ! empty( $external_url ) ) : ?><span class="external-url"><a href="<?php echo esc_url( $external_url ); ?>"><?php echo esc_url( $external_url ); ?></a></span><?php endif; ?>
+			<div class="timeline-content timeline-content-<?php echo $column_class; ?>">
+				<?php
+				$content = apply_filters( 'the_content', get_the_content() );
+				$content = preg_replace( '@<(script|style)[^>]*?>.*?</\\1>@si', '', $content );
+				$content = strip_tags( $content, '<p><a><span><div><strong><em><b><i><sup><sub><ul><li><h1><h2><h3><h4><h5><h6>' );
+				$content = str_replace( '<p>&nbsp;</p>', '', $content );
+				$content = str_replace( '<p></p>', '', $content );
+				echo $content;
+				?>
+			</div>
 		</div>
 	</div>
 	<?php

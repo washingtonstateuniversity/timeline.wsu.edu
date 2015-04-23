362a195775916d662564f8da7fc0c397595ef2f6
jeremyfelt <jeremy.felt@gmail.com>
2015-04-23 14:09:37 -0700

    Bump version 0.7.0

--- functions.php
+++ functions.php
@@ -7,7 +7,7 @@ class WSU_Timeline_Theme {
 	/**
 	 * @var string Theme version for cache breaking.
 	 */
-	public static $version = '0.6.2';
+	public static $version = '0.7.0';
 
 	/**
 	 * Setup hooks for the theme.

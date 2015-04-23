63f4fa1af9fc8dc8b2cf82d3d8427f65b67af60c
jeremyfelt <jeremy.felt@gmail.com>
2015-04-23 15:54:39 -0700

    Setup initial scroll handling of timeline scrubber
    Fix to top on scroll, remove on other scroll

--- js/timeline.js
+++ js/timeline.js
@@ -1,1 +1,19 @@
 try{Typekit.load();}catch(e){}
+
+(function($,window){
+
+	$(document).ready(function(){
+		var $scrub = $('.scrub');
+		var scrub_top = $scrub.offset().top;
+
+		$(document).scroll(function(e){
+			var scroll_top = $(document).scrollTop();
+
+			if ( ( scrub_top - scroll_top ) <= 0 && ! $scrub.hasClass('scrub-fixed') ) {
+				$scrub.addClass('scrub-fixed');
+			} else if ( ( scrub_top - scroll_top ) > 0 && $scrub.hasClass('scrub-fixed') ) {
+				$scrub.removeClass('scrub-fixed');
+			}
+		});
+	});
+}(jQuery));

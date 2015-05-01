12973ded69863e13221527e1a0e9c4284b3ed96e
jeremyfelt <jeremy.felt@gmail.com>
2015-05-01 15:59:42 -0700

    Modify decade HTML structure for better control of image

--- front-page.php
+++ front-page.php
@@ -137,7 +137,8 @@ while( $timeline_query->have_posts() ) {
 
 		if ( isset( $timeline_decades[ $timeline_decade ] ) ) {
 			?>
-			<div class="decade-item" style="background-image: url(<?php echo esc_url( $timeline_decades[ $timeline_decade ]['image'] ); ?>);">
+			<div class="decade-item">
+				<div class=“decade-image" style="background-image: url(<?php echo esc_url( $timeline_decades[ $timeline_decade ]['image'] ); ?>);">
 				<span class="decade-item-title"><?php echo esc_html( $timeline_decades[ $timeline_decade ]['title'] ); ?></span>
 				<div class="decade-item-content">
 					<?php echo $timeline_decades[ $timeline_decade ]['content']; ?>
